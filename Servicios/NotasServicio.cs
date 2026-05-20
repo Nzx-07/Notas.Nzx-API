b199cf8c7f4135e6d8e6d26717216e9e2c3b016d
Nzx-07 <bolivarnadir@gmail.com>
2026-05-20 17:11:48 -0500

    Fix: Correcion de variable en funsion Crear de NotasServicio.cs

--- Servicios/NotasServicio.cs
+++ Servicios/NotasServicio.cs
@@ -33,13 +33,11 @@ public class NotasServicio(AppDbContext db) : INotasServicio
     }
 
     public async Task<(NotaRespuesta? Nota, string? Error)> Crear(string titulo, string contenido, Guid usuarioId)
-    {
-        Contenido = contenido?.Trim() ?? "",
-        
+    {        
         var nota = new Nota
         {
             Titulo = titulo.Trim(),
-            Contenido = contenido.Trim(),
+            Contenido = contenido.Trim() ?? "",
             UsuarioId = usuarioId
         };
 
